e70a775167c25e4307e8213e8891bd330de145e1
Ulises Rosales <ulises223366@gmail.com>
2025-06-01 00:28:35 -0300

    desarrollo de estructura básica

---
+++ Back-End/agrotrace/src/test/java/com/hackathon/agrotrace/AgrotraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.agrotrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrotraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
